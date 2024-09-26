9f6257073a85a7c976d93f6d4480bc5b55dace39
Manoj <manojpolevault1202@gmail.com>
2024-09-26 13:37:38 +0530

    Final

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -9,9 +9,10 @@ export default function Experience() {
   <div className="flex-grow-1">
     <h3 className="mb-0 text-primary">React Developer Intern</h3>
     <div className="mb-2 text-muted" style={{ fontSize: '1.3rem' }}>BlueKode Academy Pvt. Ltd.</div>
-
+    <div style={{fontSize:'1.2rem', marginBottom:"10px"}}><a href="https://drive.google.com/file/d/1rT0J5D7PGS7FPIEEJFYQNWbre-40VAhB/view">Completed Certificate</a></div>
     <p className="mb-0">
-      <strong className="d-block mb-2">Roles/Responsibilities:</strong>
+      <strong className="d-block mb-1">Roles/Responsibilities:</strong>
+     
       <ul className="list-unstyled mb-0">
         <li className="mb-1"><i className="bi bi-check-circle text-success"></i> Responsible for handling the frontend.</li>
         <li className="mb-1"><i className="bi bi-check-circle text-success"></i> Implement new features on the website.</li>
@@ -21,7 +22,7 @@ export default function Experience() {
     </p>
   </div>
   <div className="flex-shrink-0 d-flex align-items-center">
-    <span className="text-primary fw-bold fs-5">June 2024 - Present</span>
+    <span className="text-primary fw-bold fs-5">June 2024 - August 2024</span>
   </div>
 </div>
 
